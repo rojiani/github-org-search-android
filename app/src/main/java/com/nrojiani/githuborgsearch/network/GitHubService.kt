cc005b38c72dab2a898f695efd4838a6d03672ec
Navid Rojiani <nrojiani@gmail.com>
2019-03-18 02:01:13 -0400

    Adapt GitHubService (fetch Org details instead of Repos)

--- app/src/main/java/com/nrojiani/githuborgsearch/network/GitHubService.kt
+++ app/src/main/java/com/nrojiani/githuborgsearch/network/GitHubService.kt
@@ -1,5 +1,6 @@
 package com.nrojiani.githuborgsearch.network
 
+import com.nrojiani.githuborgsearch.model.Organization
 import com.nrojiani.githuborgsearch.model.Repo
 import retrofit2.Call
 import retrofit2.http.GET
@@ -8,15 +9,18 @@ import retrofit2.http.Path
 
 interface GitHubService {
 
+    /**
+     * Search for an organization.
+     */
+    @Headers("Accept: application/vnd.github.v3+json", "Content-Type: application/json")
+    @GET("orgs/{org}")
+    fun getOrg(@Path("org") org: String): Call<Organization>
+
+    /**
+     * Fetch the list of repositories associated with an organization.
+     */
     @Headers("Accept: application/vnd.github.v3+json", "Content-Type: application/json")
     @GET("orgs/{org}/repos")
     fun getRepositoriesForOrg(@Path("org") org: String): Call<List<Repo>>
-
-    @Headers("Accept: application/vnd.github.v3+json", "Content-Type: application/json")
-    @GET("repos/{owner}/{name}")
-    fun getRepo(
-        @Path("owner") repoOwner: String,
-        @Path("name") repoName: String
-    ): Call<Repo>
 
 }
