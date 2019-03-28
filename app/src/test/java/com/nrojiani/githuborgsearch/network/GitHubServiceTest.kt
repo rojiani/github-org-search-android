b9684b39dd87b075491ebc8cd7cc7374ef73067d
Navid Rojiani <nrojiani@gmail.com>
2019-03-28 02:00:18 -0400

    Finish GitHubServiceTest

--- app/src/test/java/com/nrojiani/githuborgsearch/network/GitHubServiceTest.kt
+++ app/src/test/java/com/nrojiani/githuborgsearch/network/GitHubServiceTest.kt
@@ -1,6 +1,7 @@
 package com.nrojiani.githuborgsearch.network
 
 import com.nrojiani.githuborgsearch.model.Organization
+import com.nrojiani.githuborgsearch.model.Repo
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import org.junit.Assert.*
@@ -14,12 +15,18 @@ private const val ORG_NAME = "nytimes"
 
 class GitHubServiceTest : MockWebServerTest() {
 
+    private val expectedHeaders = mapOf(
+        "Accept" to "application/vnd.github.v3+json",
+        "Content-Type" to "application/json"
+    )
+
     private enum class MockApiResponse(val filename: String, val code: Int) {
         ORG_SUCCESS("org-nytimes.json", 200),
         ORG_SUCCESS_WITH_MISSING_AND_NULL_PROPS("org-amzn.json", 200),
         ORG_NOT_FOUND("org-404.json", 404),
-        REPO_SUCCESS("repo-nytimes.json", 200),
-        REPO_NOT_FOUND("repo-404.json", 404)
+        REPOS_SUCCESS("repos-nytimes.json", 200),
+        REPOS_SUCCESS_WITH_MISSING_AND_NULL_PROPS("repos-amzn.json", 200),
+        REPOS_NOT_FOUND("repos-404.json", 404)
     }
 
     private lateinit var gitHubClient: GitHubService
@@ -43,14 +50,9 @@ class GitHubServiceTest : MockWebServerTest() {
     }
 
     @Test
-    fun sendsAcceptAndContentTypeHeaders() {
+    fun `expected headers sent with getOrg request`() {
         enqueueMockApiResponse(MockApiResponse.ORG_SUCCESS)
         gitHubClient.getOrg(ORG_NAME).execute()
-
-        val expectedHeaders = mapOf(
-            "Accept" to "application/vnd.github.v3+json",
-            "Content-Type" to "application/json"
-        )
         assertRequestContainsHeaders(expectedHeaders)
     }
 
@@ -58,7 +60,7 @@ class GitHubServiceTest : MockWebServerTest() {
     fun `getOrg call sends to correct resource path endpoint`() {
         enqueueMockApiResponse(MockApiResponse.ORG_SUCCESS)
         gitHubClient.getOrg(ORG_NAME).execute()
-        assertGetRequestSentTo("/orgs/$ORG_NAME")
+        assertRequestSentTo("/orgs/$ORG_NAME")
     }
 
     @Test
@@ -96,15 +98,99 @@ class GitHubServiceTest : MockWebServerTest() {
         assertNotNull(org)
 
         org?.apply {
-            assertEquals(name, "Amazon")
-            assertEquals(login, "amzn")
-            assertEquals(avatarUrl, "https://avatars1.githubusercontent.com/u/8594673?v=4")
-            assertEquals(blogUrl, "")
-            assertEquals(description, "")
+            assertEquals("Amazon", name)
+            assertEquals("amzn", login)
+            assertEquals("https://avatars1.githubusercontent.com/u/8594673?v=4", avatarUrl)
+            assertEquals("", blogUrl)
+            assertEquals("", description)
             assertNull(location)
         }
     }
 
+    @Test
+    fun `getOrg 404 not found`() {
+        enqueueMockApiResponse(MockApiResponse.ORG_NOT_FOUND)
+        val response: Response<Organization> = gitHubClient.getOrg("foobar").execute()
+        assertNull(response.body())
+        assertEquals(404, response.code())
+
+        /* response.message() would be "Not Found", but MockResponse doesn't expose a way
+           to set the message, and it is set based on HTTP status code category (1xx, 2xx, ..., 5xx).
+           code: http://tinyurl.com/y5hx4pge */
+        assertEquals("Client Error", response.message())
+    }
+
+    @Test
+    fun `expected headers sent with getRepositoriesForOrg request`() {
+        enqueueMockApiResponse(MockApiResponse.REPOS_SUCCESS)
+        gitHubClient.getRepositoriesForOrg(ORG_NAME).execute()
+        assertRequestContainsHeaders(expectedHeaders)
+    }
+
+
+    @Test
+    fun `getRepositoriesForOrg call sends to correct resource path endpoint`() {
+        enqueueMockApiResponse(MockApiResponse.REPOS_SUCCESS)
+        gitHubClient.getRepositoriesForOrg(ORG_NAME).execute()
+        assertRequestSentTo("/orgs/$ORG_NAME/repos")
+    }
+
+    @Test
+    fun `getRepositoriesForOrg sends a GET request`() {
+        enqueueMockApiResponse(MockApiResponse.REPOS_SUCCESS)
+        gitHubClient.getRepositoriesForOrg(ORG_NAME).execute()
+        assertIsExpectedHttpRequestMethod(HttpRequestMethod.GET)
+    }
+
+    @Test
+    fun `getRepositoriesForOrg deserialized to List of type Repo`() {
+        enqueueMockApiResponse(MockApiResponse.REPOS_SUCCESS)
+        val response: Response<List<Repo>> = gitHubClient.getRepositoriesForOrg(ORG_NAME).execute()
+        val repos = response.body()
+
+        assertNotNull(repos)
+        assertEquals(30, repos!!.size)
+        assertTrue(repos is List)
+        val firstRepo = repos!!.first()
+        assertTrue(firstRepo is Repo)
+
+        firstRepo.apply {
+            assertEquals(590289, id)
+            assertEquals("tweetftp", name)
+            assertEquals("https://github.com/nytimes/tweetftp", repoUrl)
+            assertEquals(
+                "Ruby Implementation of the Tweet File Transfer Protocol (APRIL FOOLS JOKE)",
+                description
+            )
+            assertEquals(19, stars)
+            assertEquals(1, forks)
+            assertEquals("Ruby", language)
+        }
+    }
+
+    @Test
+    fun `getRepositoriesForOrg deserialized when Repo items in response hav null or empty fields`() {
+        /* In the getRepositoriesForOrg response for "Amazon", some of the Repos have language = null */
+
+        enqueueMockApiResponse(MockApiResponse.REPOS_SUCCESS_WITH_MISSING_AND_NULL_PROPS)
+        val response: Response<List<Repo>> = gitHubClient.getRepositoriesForOrg("amzn").execute()
+
+        val repos = response.body()
+        assertNotNull(repos)
+        assertEquals(30, repos!!.size)
+        assertTrue(repos is List)
+        val firstRepo = repos!!.first()
+        assertTrue(firstRepo is Repo)
+    }
+
+    @Test
+    fun `getRepositoriesForOrg 404 not found`() {
+        enqueueMockApiResponse(MockApiResponse.REPOS_SUCCESS_WITH_MISSING_AND_NULL_PROPS)
+        val response: Response<List<Repo>> = gitHubClient.getRepositoriesForOrg("foobar").execute()
+        assertNull(response.body())
+        assertEquals(404, response.code())
+        assertEquals("Client Error", response.message())
+    }
 
     private fun enqueueMockApiResponse(mockResponse: MockApiResponse) =
         enqueueMockResponse(code = mockResponse.code, fileName = mockResponse.filename)
--- app/src/test/java/com/nrojiani/githuborgsearch/network/MockWebServerTest.kt
+++ app/src/test/java/com/nrojiani/githuborgsearch/network/MockWebServerTest.kt
@@ -1,29 +1,19 @@
 package com.nrojiani.githuborgsearch.network
 
-import android.util.Log
 import com.nrojiani.githuborgsearch.testing.readMockApiResponse
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import okhttp3.mockwebserver.RecordedRequest
-import org.hamcrest.core.StringContains
 import org.junit.After
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertThat
 import org.junit.Before
-import java.io.File
 
 
 /**
- * Adapted from [GitHub: Karumi/KataTODOApiClientKotlin](https://github.com/Karumi/KataTODOApiClientKotlin)
+ * Adapted from:
  * [GitHub: Karumi/KataTODOApiClientKotlin - MockWebServerTest.kt](https://github.com/Karumi/KataTODOApiClientKotlin/blob/master/src/test/kotlin/com/karumi/todoapiclient/MockWebServerTest.kt)
  */
 open class MockWebServerTest {
-
-    private val TAG by lazy { this::class.java.simpleName }
-
-    companion object {
-        private val FILE_ENCODING = Charsets.UTF_8
-    }
 
     private var server: MockWebServer = MockWebServer()
 
@@ -47,77 +37,25 @@ open class MockWebServerTest {
     fun enqueueMockResponse(code: Int = 200, fileName: String? = null) {
         val mockResponse = MockResponse()
         val fileContent = readMockApiResponse(fileName!!)
-        mockResponse.setResponseCode(code)
         mockResponse.setBody(fileContent)
+        mockResponse.setResponseCode(code)
         server.enqueue(mockResponse)
-    }
-
-    protected fun assertRequestSentTo(url: String) {
-        val request = server.takeRequest()
-        assertEquals(url, request.path)
     }
 
     protected fun assertIsExpectedHttpRequestMethod(httpMethod: HttpRequestMethod) {
         val request = server.takeRequest()
         assertEquals(httpMethod.name, request.method)
     }
-    
-    protected fun assertGetRequestSentTo(url: String) {
+
+    protected fun assertRequestSentTo(url: String) {
         val request = server.takeRequest()
         assertEquals(url, request.path)
-        assertEquals("GET", request.method)
-    }
-
-    protected fun assertPostRequestSentTo(url: String) {
-        val request = server.takeRequest()
-        assertEquals(url, request.path)
-        assertEquals("POST", request.method)
-    }
-
-    protected fun assertPutRequestSentTo(url: String) {
-        val request = server.takeRequest()
-        assertEquals(url, request.path)
-        assertEquals("PUT", request.method)
-    }
-
-    protected fun assertDeleteRequestSentTo(url: String) {
-        val request = server.takeRequest()
-        assertEquals(url, request.path)
-        assertEquals("DELETE", request.method)
-    }
-
-    protected fun assertRequestSentToContains(vararg paths: String) {
-        val request = server.takeRequest()
-
-        for (path in paths) {
-            assertThat(request.path, StringContains.containsString(path))
-        }
     }
     
     fun assertRequestContainsHeaders(headers: Map<String, String>, requestIndex: Int = 0) {
         val recordedRequest = getRecordedRequestAtIndex(requestIndex)
         headers.forEach { (name, expectedValue) -> 
             assertEquals(expectedValue, recordedRequest.getHeader(name))
-        }
-    }
-
-    protected fun assertRequestBodyEquals(jsonFile: String) {
-        val request = server.takeRequest()
-        assertEquals(readMockApiResponse(jsonFile), request.body.readUtf8())
-    }
-
-    private fun getContentFromFile(fileName: String? = null): String {
-        // TODO
-        if (fileName == null) {
-            Log.d(TAG, "getContentFromFile: fileName was null")
-            return ""
-        }
-
-        val file = File(javaClass.getResource("/" + fileName).file)
-        return buildString {
-            file.useLines(FILE_ENCODING) { lines ->
-                lines.forEach { append("$it") }
-            }
         }
     }
 
